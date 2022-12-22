79e7b404e867080cc2d6bbdf2d5697a5ec3bc8c6
Egor Solonin <e.solonin@onpoint.ru>
2022-12-22 19:42:14 +0300

    add selcet queries

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ import {
     insertTables,
     dropTable,
     createAllCSV,
+    createCSV,
 } from './src/functions.js'
+import {
+    selectBySpecialty,
+    selectOnExperience,
+    selectPolyclinicMSK,
+    selectOrgsFilteredBySpec,
+    selectOnRegion,
+} from './sql/select.js'
 
 // https://www.w3schools.com/nodejs/nodejs_mysql.asp
 
@@ -21,4 +29,15 @@ con.connect(function (err) {
     // createTables(con)
     // insertTables(con)
     // createAllCSV(con)
+
+    // 1
+    // createCSV(con, selectBySpecialty('челюстно-лицевой хирург'), `select/1.csv`)
+    // 2
+    // createCSV(con, selectOnExperience(10), `select/2.csv`)
+    // 3
+    // createCSV(con, selectPolyclinicMSK(), `select/3.csv`)
+    // 4
+    // createCSV(con, selectOrgsFilteredBySpec('стоматолог-хирург'), `select/4.csv`)
+    // 5
+    // createCSV(con, selectOnRegion('Санкт-Петербург'), `select/5.csv`)
 })
--- sql/select.js
+++ sql/select.js
@@ -5,6 +5,79 @@ const selectAllOrgs = `SELECT * FROM organizations`
 const selectAllOrgTypes = `SELECT * FROM organizationTypes`
 const selectAllRegions = `SELECT * FROM regions`
 
+const selectBySpecialty = specialty => `
+    SELECT 
+        doctors.firstName AS name,
+        doctors.email AS email,
+        subSpecialties.name AS specialty
+    FROM doctors
+    INNER JOIN subSpecialties 
+    ON ( doctors.specialtyId = subSpecialties.id )
+    WHERE subSpecialties.name = '${specialty}'
+    AND doctors.email != ''
+`
+
+const selectOnExperience = exp => `
+    SELECT 
+        doctors.firstName AS name,
+        doctors.email AS email,
+        doctors.experience AS experience
+    FROM doctors
+    WHERE doctors.experience > '${exp}'
+    AND doctors.email != ''
+    ORDER BY experience DESC
+`
+
+const selectPolyclinicMSK = () => `
+    SELECT 
+        doctors.firstName AS name,
+        organizations.name AS orgName,
+        regions.name AS region
+    FROM doctors
+
+    RIGHT JOIN organizations
+    ON doctors.organizationId = organizations.id
+
+    RIGHT JOIN organizationTypes
+    ON organizations.orgTypeId = organizationTypes.id
+
+    RIGHT JOIN regions
+    ON regions.id = doctors.regionId
+
+    WHERE regions.name = 'Москва'
+    AND organizationTypes.name = 'Поликлиника'
+`
+
+const selectOrgsFilteredBySpec = specialty => `
+    SELECT organizations.name AS orgName FROM organizations
+
+    WHERE organizations.id NOT IN (
+        SELECT 
+            organizations.id AS orgId
+        FROM organizations
+
+        INNER JOIN doctors
+        ON doctors.organizationId = organizations.id
+
+        INNER JOIN subSpecialties
+        ON doctors.specialtyId = subSpecialties.id
+        
+        WHERE subSpecialties.name = '${specialty}'
+        GROUP BY orgId
+    )
+`
+
+const selectOnRegion = region => `
+    SELECT  
+        doctors.firstName AS name,
+        regions.name AS region
+    FROM doctors
+    RIGHT JOIN regions
+    ON regions.id = doctors.regionId
+
+    WHERE regions.name = '${region}'
+`
+
 export {
     selectAllDocs,
     selectAllSubSpecs,
@@ -12,4 +85,9 @@ export {
     selectAllOrgs,
     selectAllOrgTypes,
     selectAllRegions,
+    selectBySpecialty,
+    selectOnExperience,
+    selectPolyclinicMSK,
+    selectOrgsFilteredBySpec,
+    selectOnRegion,
 }
